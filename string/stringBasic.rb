d25479db42f62b6f0ade426e0552f4c426f52999
Zahid Iqbal Tushar <zi.tushar@live.com>
2019-07-06 13:36:32 +0600

    added variable and string

---
+++ string/stringBasic.rb
@@ -0,0 +1,15 @@
+stringVariable = "This a sample string";
+
+puts stringVariable;
+
+
+concat = "this" + "is" + "a" + "string";
+puts concat;
+
+blah = "blah" * 3;
+puts blah;
+
+puts concat.upcase;
+puts concat.capitalize;
+puts concat.length;
+puts concat.reverse;
---
+++ variable/variable.rb
@@ -0,0 +1,15 @@
+int_variable = 5;
+
+puts int_variable;
+puts '';
+puts 'with string concat and convert integer to string: ' + int_variable.to_s;
+
+var = 2+5+8*6;
+puts 'output var: ' + var.to_s;
+
+thousand = 1234.56;
+puts 'thousands: ' + thousand.to_s;
+
+# separate thousands, say what!!!!!!!!!!!!!!!!!!!!
+thousand_1 = 1_234.56;
+puts 'thousand_1: ' + thousand_1.to_s;
