8b460faecd98548598062de317bb44023f7b0372
Zahid Iqbal Tushar <zi.tushar@live.com>
2019-07-07 22:01:42 +0600

    updated objectBasic.rb

--- oop/objectBasic.rb
+++ oop/objectBasic.rb
@@ -15,3 +15,28 @@ puts "ruby tutorials".prepend("hi ").upcase;
 
 puts 1.odd?;
 puts 1.even?;
+
+puts"----- between"
+puts 56.between?(0, 99);
+
+puts "----- zero";
+puts 0.zero?;
+puts 1.zero?;
+
+puts "----- array";
+puts [1, 2].include?(1);
+puts [1, 2].include?(3);
+
+# puts "----- dictionary";
+# puts { "uno" => "one" }.key?("uno");
+# puts { "eins" => "one" }.key?("zwei");
+
+puts "----- bang methods"
+name = "SnowWolf";
+puts name.downcase;
+puts name;
+
+#carefull with this...
+anotherName = "SnowFox";
+puts anotherName.downcase!; # this will actually change the value of the variable
+puts anotherName;
